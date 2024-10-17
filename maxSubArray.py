c1bd8d835df02fa6fe204ef2c2c4f371c312cdc4
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2024-10-17 16:41:09 +0530

    maxSubArray added

---
+++ maxSubArray.py
@@ -0,0 +1,16 @@
+nums = [-1]
+
+res = nums[0]
+maxPreviousSum = 0
+startIndex = 0
+endIndex = len(nums) - 1
+for i in range(len(nums)):
+    maxPreviousSum = maxPreviousSum + nums[i]
+    if(maxPreviousSum < nums[i]):
+        startIndex = i
+        maxPreviousSum = nums[i]
+    if(maxPreviousSum>res):
+        res = maxPreviousSum
+        endIndex = i
+    # print(maxPreviousSum, res)
+print(res, nums[startIndex:endIndex+1])
