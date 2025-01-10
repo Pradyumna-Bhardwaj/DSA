0d237800f716a42ffe7be699b7a04354c3aabd5f
Pradyumna <bhardwaj.pradyumn.pb@gmail.com>
2025-01-10 12:07:23 +0530

    linked list completed and BT added

---
+++ binaryTree/maxDepth.py
@@ -0,0 +1,47 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def preOrderTraversal(node):
+    if node is None:
+        return
+    print(node.val, end=", ")
+    preOrderTraversal(node.left)
+    preOrderTraversal(node.right)
+
+root = TreeNode('R')
+nodeA = TreeNode('A')
+nodeB = TreeNode('B')
+nodeC = TreeNode('C')
+nodeD = TreeNode('D')
+nodeE = TreeNode('E')
+nodeF = TreeNode('F')
+nodeG = TreeNode('G')
+
+root.left = nodeA
+root.right = nodeB
+
+nodeA.left = nodeC
+nodeA.right = nodeD
+
+nodeB.left = nodeE
+nodeB.right = nodeF
+
+nodeF.left = nodeG
+
+# Traverse
+# preOrderTraversal(root)
+
+def maxDepth(node):
+    if node == None:
+        return -1
+    lHeight = maxDepth(node.left)
+    rHeight = maxDepth(node.right)
+
+    return max(lHeight, rHeight) + 1
+
+maxHeight = maxDepth(root)
+
+print(maxHeight)
